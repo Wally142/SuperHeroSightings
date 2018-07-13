9fbc08c397395967efc1c80476392f5eef7e047b
Greg.Wallerus <gwallerus@gmail.com>
2018-07-13 13:24:59 -0500

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/SuperHero-1.0.1.jar
+web: java -jar target/SuperHero-1.0.1.jar --port $PORT target/*.war
--- src/main/java/com/sg/superherosightings/data/MySqlDatabase.java
+++ src/main/java/com/sg/superherosightings/data/MySqlDatabase.java
@@ -18,37 +18,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class MySqlDatabase {
 
-//    @Bean
-//    public DataSourceTransactionManager getTransactionManager(DataSource ds) {
-//        return new DataSourceTransactionManager(ds);
-//    }
-
     @Bean
-    public JdbcTemplate getJdbcTemplate(Connection conn) {
-        return new JdbcTemplate((DataSource) conn);
+    public DataSourceTransactionManager getTransactionManager(DataSource ds) {
+        return new DataSourceTransactionManager(ds);
     }
 
-//    @Bean
-//    public static DataSource getDataSource() throws SQLException {
-//
-//        Properties dbProperties = new Properties();
-//        try (InputStream stream = MySqlDatabase.class.getClassLoader().getResourceAsStream("db.properties")) {
-//            dbProperties.load(stream);
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//            System.exit(100);
-//        }
-//
-//        MysqlDataSource ds = new MysqlDataSource();
-//        ds.setServerName(dbProperties.getProperty("serverName"));
-//        ds.setDatabaseName(dbProperties.getProperty("databaseName"));
-//        ds.setUser(dbProperties.getProperty("userName"));
-//        ds.setPassword(dbProperties.getProperty("password"));
-//        ds.setServerTimezone("UTC");
-//        ds.setUseSSL(false);
-//
-//        return ds;
-//    }
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource ds) {
+        return new JdbcTemplate(ds);
+    }
+
+    @Bean
+    public static DataSource getDataSource() throws SQLException {
+
+        Properties dbProperties = new Properties();
+        try (InputStream stream = MySqlDatabase.class.getClassLoader().getResourceAsStream("db.properties")) {
+            dbProperties.load(stream);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(100);
+        }
+
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setServerName(dbProperties.getProperty("serverName"));
+        ds.setDatabaseName(dbProperties.getProperty("databaseName"));
+        ds.setUser(dbProperties.getProperty("userName"));
+        ds.setPassword(dbProperties.getProperty("password"));
+        ds.setServerTimezone("UTC");
+        ds.setUseSSL(false);
+
+        return ds;
+    }
 
     @Bean
     private static Connection getConnection() throws URISyntaxException, SQLException {
