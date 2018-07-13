8044e461d7b535658e2149b378b84a0864be1d08
Greg.Wallerus <gwallerus@gmail.com>
2018-07-12 21:05:51 -0500

    connection

--- src/main/java/com/sg/superherosightings/data/MySqlDatabase.java
+++ src/main/java/com/sg/superherosightings/data/MySqlDatabase.java
@@ -1,11 +1,13 @@
-
 package com.sg.superherosightings.data;
-
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.DriverManager;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
@@ -26,25 +28,37 @@ public class MySqlDatabase {
         return new JdbcTemplate(ds);
     }
 
+//    @Bean
+//    public static DataSource getDataSource() throws SQLException {
+//
+//        Properties dbProperties = new Properties();
+//        try (InputStream stream = MySqlDatabase.class.getClassLoader().getResourceAsStream("db.properties")) {
+//            dbProperties.load(stream);
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//            System.exit(100);
+//        }
+//
+//        MysqlDataSource ds = new MysqlDataSource();
+//        ds.setServerName(dbProperties.getProperty("serverName"));
+//        ds.setDatabaseName(dbProperties.getProperty("databaseName"));
+//        ds.setUser(dbProperties.getProperty("userName"));
+//        ds.setPassword(dbProperties.getProperty("password"));
+//        ds.setServerTimezone("UTC");
+//        ds.setUseSSL(false);
+//
+//        return ds;
+//    }
+
     @Bean
-    public static DataSource getDataSource() throws SQLException {
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
 
-        Properties dbProperties = new Properties();
-        try (InputStream stream = MySqlDatabase.class.getClassLoader().getResourceAsStream("db.properties")) {
-            dbProperties.load(stream);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            System.exit(100);
-        }
+        String username = jdbUri.getUserInfo().split(":")[0];
+        String password = jdbUri.getUserInfo().split(":")[1];
+        String port = String.valueOf(jdbUri.getPort());
+        String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
 
-        MysqlDataSource ds = new MysqlDataSource();
-        ds.setServerName(dbProperties.getProperty("serverName"));
-        ds.setDatabaseName(dbProperties.getProperty("databaseName"));
-        ds.setUser(dbProperties.getProperty("userName"));
-        ds.setPassword(dbProperties.getProperty("password"));
-        ds.setServerTimezone("UTC");
-        ds.setUseSSL(false);
-
-        return ds;
+        return DriverManager.getConnection(jdbUrl, username, password);
     }
 }
